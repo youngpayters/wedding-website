b23ce51e3e375e6cac48a91f2099112980b48740
youngpayters <darylpet@gmail.com>
2023-06-23 19:48:13 +0100

    adding countdown timer
    - some emojis
    - text fixes

--- js/scripts.js
+++ js/scripts.js
@@ -536,3 +536,34 @@ var MD5 = function (string) {
 
   return temp.toLowerCase();
 };
+
+// Countdown timer
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 23, 2023 14:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  $("#countdown").html(
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+  );
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    $("#countdown").html("It's time to get hitched!");
+  }
+}, 1000);
